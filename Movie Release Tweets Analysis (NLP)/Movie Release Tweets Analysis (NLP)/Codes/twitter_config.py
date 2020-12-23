4d6edaa13456dbb5e79a76d2d2504ea8be496457
Adu Boahene Q <57592697+QABoahene@users.noreply.github.com>
2020-12-23 21:53:30 +0000

    Delete twitter_config.py

--- Movie Release Tweets Analysis (NLP)/Movie Release Tweets Analysis (NLP)/Codes/twitter_config.py
+++
@@ -1,8 +0,0 @@
-consumer_key = 'ex9taKKpQiIA7ySrb3xqpDrTp'
-consumer_secret = '0Kpv9isbRm1SmbFTQlK7N0YIs0Us0RAMzCli11NPdgttX9LDFm'
-access_token = '324932965-8TWwjQxnCRSDv0S0yOGkB5xrEY10LV5xCRqS3uHJ'
-access_token_secret = '2LODZHJWZ3XkiaPsRkLVKt6MywVoa6TOjJOosPCUR9XdY'
-
-#Google sheet credentials
-file_name = 'Tweets'
-google_sheet_name = '16KZZeOmgievJgto6mU6oAc_g37JAnvUXBCGSR93mp50'
